6aaf9e9f225a93da5856b73d84b4beab08c27de7
ha39-cell <alshayjiha@gmail.com>
2026-04-23 10:38:03 +0300

    Add files via upload

---
+++ wazenApp.swift
@@ -0,0 +1,17 @@
+//
+//  wazenApp.swift
+//  wazen
+//
+//  Created by جنى عبدالله الشبانات on 01/07/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct wazenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
